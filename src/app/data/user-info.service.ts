9218a836cabedf3fdbe83623caf30922d6527714
javiercillo_11 <javiercillo2001@gmail.com>
2024-03-04 19:01:08 +0100

    10 cards

--- src/app/data/user-info.service.ts
+++ src/app/data/user-info.service.ts
@@ -1,18 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { Observable, map, of } from 'rxjs';
+import { Observable, forkJoin, map, of } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class UserInfoService {
-//1,183
-    private ApiURL = 'https://rickandmortyapi.com/api/character/540';
-    constructor(private http: HttpClient) {}
+    private http = inject(HttpClient); 
+    private ApiURL = 'https://rickandmortyapi.com/api/character/540,3,4,5,6,8,9,11,12,13';
+    //private ApiURL2 = 'https://rickandmortyapi.com/api/character/2';
+    constructor() {}
 
     public getData(): Observable<any[]> {
-        return this.http.get<any>(this.ApiURL).pipe(
-            map(data => [data])
-        );
-}
+        const req1 = this.http.get<any>(this.ApiURL);
+        //const req2 = this.http.get<any>(this.ApiURL2);
+        return forkJoin([req1]).pipe(
+            map(([rest1])=> {
+                return [...rest1];
+            })
+        )
+    }
 }
 export interface ApiResponse {
     name: string;
