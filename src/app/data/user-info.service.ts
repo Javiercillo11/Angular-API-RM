903ffe8a77c24b117c445eaff8f5c8ca1b4782fe
javiercillo_11 <javiercillo2001@gmail.com>
2024-03-05 19:11:10 +0100

    cambio

--- src/app/data/user-info.service.ts
+++ src/app/data/user-info.service.ts
@@ -5,7 +5,7 @@ import { Observable, forkJoin, map, of } from 'rxjs';
 @Injectable({providedIn: 'root'})
 export class UserInfoService {
     private http = inject(HttpClient); 
-    private ApiURL = 'https://rickandmortyapi.com/api/character/540,3,4,5,6,8,9,11,12,13,14,15,16,17,18,20,21,22,23,24';
+    private ApiURL = 'https://rickandmortyapi.com/api/character/540,1,2,3,4,8,9,11,12,13,14,15,16,17,18,20,21,22,23,24';
     //private ApiURL2 = 'https://rickandmortyapi.com/api/character/2';
     constructor() {}
 
